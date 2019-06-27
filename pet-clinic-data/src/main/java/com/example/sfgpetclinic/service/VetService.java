fc4cb33ef35260e6ac72974e9a9b003c4fba2952
Jason Motee <jason.motee@autotrader.co.uk>
2019-06-27 15:28:09 +0100

    Implement Interface for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/service/VetService.java
@@ -0,0 +1,14 @@
+package com.example.sfgpetclinic.service;
+
+import com.example.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet owner);
+
+    Set<Vet> findAll();
+}
