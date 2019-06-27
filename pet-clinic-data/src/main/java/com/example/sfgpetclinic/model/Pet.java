fb235abdd0e0f3e61f6a1df4d5426676d200f2ab
Jason Motee <jason.motee@autotrader.co.uk>
2019-06-27 15:12:53 +0100

    refactoring to multimodule maven project. closes #1.

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Pet.java
@@ -6,7 +6,7 @@ public class Pet {
 
     private PetType petType;
     private Owner owner;
-    LocalDate birthDate;
+    private LocalDate birthDate;
 
     public PetType getPetType() {
         return petType;
